9651b95f64a47ff42b8830930686641a57be2601
TalalMajeed <m.talal.majeed@gmail.com>
2024-10-05 21:32:49 +0500

    Updated Logo

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,5 +1,5 @@
 import "./index.scss";
 import Loader from "../../features/loader/loader";
 export default function Index() {
-    return <Loader width={"100svw"} height={"100svh"} />;
+    return <Loader type={""} width={"100svw"} height={"100svh"} />;
 }
