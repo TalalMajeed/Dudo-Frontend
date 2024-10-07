6c87c9d07d95edbd85abfd621efe0837423e070e
TalalMajeed <m.talal.majeed@gmail.com>
2024-10-07 12:36:16 +0500

    Added FavIcon FIx

--- src/pages/Panel/index.jsx
+++ src/pages/Panel/index.jsx
@@ -1,9 +1,7 @@
 export default function Panel() {
     return (
         <div className="panel">
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+            <div>Coming Soon!!</div>
         </div>
     );
 }
